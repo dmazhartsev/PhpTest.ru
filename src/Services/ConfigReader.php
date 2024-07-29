8f1b0a2e4cd78ffbda158d3920eb84e80be279cf
dmazhartsev <den-nety@mail.ru>
2024-07-29 23:07:17 +0300

    let3

--- src/Services/ConfigReader.php
+++ src/Services/ConfigReader.php
@@ -2,7 +2,22 @@
 
 namespace App\Services;
 
+use Symfony\Component\Yaml\Yaml;
+
 class ConfigReader
 {
+    private Yaml $yamlParser;
+    private string $filePath;
+
+    public function __construct()
+    {
+        $this->yamlParser = new Yaml();
+        $this->filePath = __DIR__ . '/../../config/config.yml';
+    }
+
+    public function read(): array
+    {
+        return $this->yamlParser->parseFile($this->filePath);
+    }
 
 }
--- src/Services/MessageService/Senders/EmailSender.php
+++ src/Services/MessageService/Senders/EmailSender.php
@@ -2,32 +2,41 @@
 
 namespace App\Services\MessageService\Senders;
 
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+use App\Services\ConfigReader;
 use PHPMailer\PHPMailer\PHPMailer;
 
-class SMTPMailer
+class EmailSender
 {
-    private string $host;
+    private ConfigReader $configReader;
+    private array $config;
 
-    public function send(string $subject, string $message, string $from, string $to)
+    public function __construct()
+    {
+        $this->configReader = new ConfigReader();
+        $this->config = $this->configReader->read()['smtp'];
+    }
+
+    public function send(string $subject, string $message, string | array $to): void
     {
         $mail = new PHPMailer();
+        $mail->CharSet = 'UTF-8';
         $mail->isSMTP();
-        $mail->Host = $this->host;
+        $mail->Host = $this->config['Host'];
         $mail->SMTPAuth = true;
-        $mail->Username = 'username';
-        $mail->Password = 'password';
-        $mail->SMTPSecure = 'ssl';
-        $mail->Port = 465;
-        $mail->setFrom($from);
+        $mail->Username = $this->config['Username'];
+        $mail->Password = $this->config['Password'];
+        $mail->SMTPSecure = $this->config['Encryption'];
+        $mail->Port = $this->config['Port'];
+        $mail->setFrom($this->config['Username']);
         $mail->addAddress($to);
         $mail->isHTML(true);
         $mail->Subject = $subject;
         $mail->Body = $message;
         $mail->send();
     }
-
-    public function __construct(string $host)
-    {
-        $this->host = $host;
-    }
 }
+
+$EmailSender = new EmailSender();
+$EmailSender->send('Тема письма', 'Текст письма', 'den-nety@mail.ru', 'den-nety@mail.ru');
